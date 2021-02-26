48423789b3e3a344cefa27c2cc81b2fa05ed6e60
snowhero <hxiong@shift3tech.com>
2021-02-25 19:00:45 -0800

    init

--- src/index.js
+++ src/index.js
@@ -5,11 +5,30 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  // <React.StrictMode>
+  //   <App />
+  // </React.StrictMode>,
+
+  <div>
+    <img src="https://upload.wikimedia.org/wikipedia/en/2/20/Batman_v_Superman_poster.jpg"/>
+    <p>Batman vs Superman</p>
+  </div>,
   document.getElementById('root')
 );
+
+
+// const rootElement = document.getElementById("root");
+// const card = document.createElement('div');
+// const title = document.createElement('p');
+// const poster = document.createElement('img');
+// const type = document.createElement('p');
+
+// poster.setAttribute('src', 'https://upload.wikimedia.org/wikipedia/en/2/20/Batman_v_Superman_poster.jpg');
+
+
+// card.append(poster);
+
+// rootElement.append(card);
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
